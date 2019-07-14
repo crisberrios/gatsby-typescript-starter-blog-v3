60ba4e07b4b138f5a7593b9af8ce715f33099139
Cristian Berrios <cristian.berrios@headspring.com>
2019-07-14 02:23:48 -0500

    Update style. Add header/sidebar links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'gatsby'
 import * as React from 'react'
+import Image from '../components/image'
 
 interface HeaderProps {
   siteTitle?: string
@@ -8,9 +8,15 @@ interface HeaderProps {
 const Header: React.FunctionComponent<HeaderProps> = () => (
   <header>
     <div className="center-container">
+      <div className="margin:auto profile-image">
+        <Image />
+      </div>
+      <h1 className="font-size:lg-2">Merlo Spumi</h1>
       <nav className="max-width:50% margin:auto">
         <ul>
-          <li>Nav</li>
+          <li><a href="/">Articles</a></li>
+          <li><a href="#">About me</a></li>
+          <li><a href="#">Contact me</a></li>
         </ul>
       </nav>
     </div>
