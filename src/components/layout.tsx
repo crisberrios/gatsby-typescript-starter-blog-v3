10c8392d0a8693d3e91209466d3de9689dedc30a
Cristian Berrios <cristian.berrios@headspring.com>
2019-11-20 23:32:53 -0600

    Update dependencies

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 import Header from './header'
 
-const Layout: React.FunctionComponent<{ noSidebar: boolean }> = ({
+const Layout: React.FunctionComponent<{ noSidebar?: boolean }> = ({
   children,
   noSidebar,
 }) => (
